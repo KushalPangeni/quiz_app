c38aed753e3d518c4128b25f52eff65e7c565ece
KushalPangeni <kushalpangeni03@gmail.com>
2023-09-01 20:39:02 +0545

    upto 20 levels done

--- android/app/src/main/kotlin/com/example/quiz/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/quiz/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.quiz
+package com.millionaire.words_for_pics
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/constant/const.dart
+++ lib/constant/const.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+// 'Words starts from '
 var sizedbox1 = const SizedBox(
   height: 1,
 );
@@ -30,7 +31,8 @@ List level1 = [
   'https://images.pexels.com/photos/896567/pexels-photo-896567.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
   'https://images.pexels.com/photos/17860805/pexels-photo-17860805/free-photo-of-close-up-of-a-large-flying-fox.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
   'https://images.pexels.com/photos/7140712/pexels-photo-7140712.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-  'https://images.pexels.com/photos/9740366/pexels-photo-9740366.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+  'https://images.pexels.com/photos/9740366/pexels-photo-9740366.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  'Words starts from B'
 ];
 
 List level2 = [
@@ -41,97 +43,204 @@ List level2 = [
   'https://images.pexels.com/photos/1405355/pexels-photo-1405355.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
   'https://images.pexels.com/photos/39362/the-ball-stadion-football-the-pitch-39362.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
   'https://images.pexels.com/photos/358042/pexels-photo-358042.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-  'https://images.pexels.com/photos/1462618/pexels-photo-1462618.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+  'https://images.pexels.com/photos/1462618/pexels-photo-1462618.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  'Words starts from B'
 ];
 List level3 = [
   ['', '', '', ''],
   ['W', 'B', 'N', 'A', 'T', 'S', 'B', 'T', 'O', 'B'],
   'SNOW',
-  'What is this?',
+  '',
   'https://images.pexels.com/photos/1635088/pexels-photo-1635088.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
   "https://images.pexels.com/photos/1743379/pexels-photo-1743379.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   'https://images.pexels.com/photos/15835820/pexels-photo-15835820/free-photo-of-snowy-mountain-in-birds-eye-view.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-  'https://images.pexels.com/photos/869258/pexels-photo-869258.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+  'https://images.pexels.com/photos/869258/pexels-photo-869258.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  'Words starts from SN'
 ];
 List level4 = [
   ['', '', '', ''],
-  ['L', 'B', 'H', 'A', 'T', 'L', 'B', 'T', 'O', 'B'],
-  'BALL',
-  'What is this?',
-  'https://images.pexels.com/photos/1405355/pexels-photo-1405355.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-  'https://images.pexels.com/photos/39362/the-ball-stadion-football-the-pitch-39362.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-  'https://images.pexels.com/photos/358042/pexels-photo-358042.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-  'https://images.pexels.com/photos/1462618/pexels-photo-1462618.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+  ['F', 'R', 'H', 'O', 'O', 'L', 'S', 'T', 'O', 'E'],
+  'FOREST',
+  '',
+  'https://images.pexels.com/photos/3669275/pexels-photo-3669275.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  'https://images.pexels.com/photos/2231521/pexels-photo-2231521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  'https://images.pexels.com/photos/2894944/pexels-photo-2894944.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  'https://images.pexels.com/photos/5869023/pexels-photo-5869023.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  'Words starts from FOR'
 ];
 List level5 = [
   ['', '', '', ''],
-  ['L', 'B', 'H', 'A', 'T', 'L', 'B', 'T', 'O', 'B'],
-  'BALL',
-  'What is this?',
-  'https://images.pexels.com/photos/1405355/pexels-photo-1405355.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-  'https://images.pexels.com/photos/39362/the-ball-stadion-football-the-pitch-39362.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-  'https://images.pexels.com/photos/358042/pexels-photo-358042.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-  'https://images.pexels.com/photos/1462618/pexels-photo-1462618.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+  ['S', 'M', 'U', 'A', 'T', 'E', 'B', 'T', 'O', 'B'],
+  'MOUSE',
+  '',
+  'https://images.pexels.com/photos/2115256/pexels-photo-2115256.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  'https://images.pexels.com/photos/207272/pexels-photo-207272.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  'https://images.pexels.com/photos/2198544/pexels-photo-2198544.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  'https://images.pexels.com/photos/206627/pexels-photo-206627.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  'Words starts from MOU'
 ];
 List level6 = [
   ['', '', '', ''],
-  ['L', 'B', 'H', 'A', 'T', 'L', 'B', 'T', 'O', 'B'],
-  'BALL',
-  'What is this?',
-  'https://images.pexels.com/photos/1405355/pexels-photo-1405355.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-  'https://images.pexels.com/photos/39362/the-ball-stadion-football-the-pitch-39362.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-  'https://images.pexels.com/photos/358042/pexels-photo-358042.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-  'https://images.pexels.com/photos/1462618/pexels-photo-1462618.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+  ['L', 'I', 'H', 'E', 'T', 'L', 'B', 'M', 'O', 'B'],
+  'MOBILE',
+  '',
+  'https://images.pexels.com/photos/3121979/pexels-photo-3121979.jpeg?auto=compress&cs=tinysrgb&w=600',
+  'https://images.pexels.com/photos/5206292/pexels-photo-5206292.jpeg?auto=compress&cs=tinysrgb&w=600',
+  'https://images.pexels.com/photos/5997313/pexels-photo-5997313.jpeg?auto=compress&cs=tinysrgb&w=600',
+  'https://images.pexels.com/photos/826349/pexels-photo-826349.jpeg?auto=compress&cs=tinysrgb&w=600',
+  'Words starts from MOB'
 ];
 List level7 = [
   ['', '', '', ''],
-  ['L', 'B', 'H', 'A', 'T', 'L', 'B', 'T', 'O', 'B'],
-  'BALL',
-  'What is this?',
+  ['L', 'B', 'H', 'A', 'T', 'G', 'B', 'I', 'O', 'B'],
+  'LIGHT',
+  '',
   'https://images.pexels.com/photos/1405355/pexels-photo-1405355.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
   'https://images.pexels.com/photos/39362/the-ball-stadion-football-the-pitch-39362.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
   'https://images.pexels.com/photos/358042/pexels-photo-358042.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-  'https://images.pexels.com/photos/1462618/pexels-photo-1462618.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+  'https://images.pexels.com/photos/1462618/pexels-photo-1462618.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  'Words starts from LIGH'
 ];
 List level8 = [
   ['', '', '', ''],
-  ['L', 'B', 'H', 'A', 'T', 'L', 'B', 'T', 'O', 'B'],
-  'BALL',
-  'What is this?',
-  'https://images.pexels.com/photos/1405355/pexels-photo-1405355.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-  'https://images.pexels.com/photos/39362/the-ball-stadion-football-the-pitch-39362.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-  'https://images.pexels.com/photos/358042/pexels-photo-358042.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-  'https://images.pexels.com/photos/1462618/pexels-photo-1462618.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+  ['L', 'B', 'N', 'A', 'T', 'G', 'B', 'T', 'O', 'B'],
+  'LONG',
+  '',
+  'https://images.pexels.com/photos/3696351/pexels-photo-3696351.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  'https://images.pexels.com/photos/1382156/pexels-photo-1382156.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  'https://images.pexels.com/photos/1038935/pexels-photo-1038935.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  'https://images.pexels.com/photos/441222/pexels-photo-441222.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  'Words starts from LON'
 ];
 List level9 = [
   ['', '', '', ''],
-  ['L', 'B', 'H', 'A', 'T', 'L', 'B', 'T', 'O', 'B'],
-  'BALL',
-  'What is this?',
-  'https://images.pexels.com/photos/1405355/pexels-photo-1405355.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-  'https://images.pexels.com/photos/39362/the-ball-stadion-football-the-pitch-39362.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-  'https://images.pexels.com/photos/358042/pexels-photo-358042.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-  'https://images.pexels.com/photos/1462618/pexels-photo-1462618.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+  ['L', 'B', 'H', 'I', 'T', 'R', 'B', 'T', 'G', 'B'],
+  'BRIGHT',
+  '',
+  'https://images.pexels.com/photos/568948/pexels-photo-568948.jpeg?auto=compress&cs=tinysrgb&w=600',
+  'https://images.pexels.com/photos/1161547/pexels-photo-1161547.jpeg?auto=compress&cs=tinysrgb&w=600',
+  'https://images.pexels.com/photos/1164985/pexels-photo-1164985.jpeg?auto=compress&cs=tinysrgb&w=600',
+  'https://images.pexels.com/photos/1496373/pexels-photo-1496373.jpeg?auto=compress&cs=tinysrgb&w=600',
+  'Words starts from BRIGH'
 ];
 List level10 = [
   ['', '', '', ''],
-  ['L', 'B', 'H', 'A', 'T', 'L', 'B', 'T', 'O', 'B'],
-  'BALL',
-  'What is this?',
-  'https://images.pexels.com/photos/1405355/pexels-photo-1405355.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-  'https://images.pexels.com/photos/39362/the-ball-stadion-football-the-pitch-39362.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-  'https://images.pexels.com/photos/358042/pexels-photo-358042.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-  'https://images.pexels.com/photos/1462618/pexels-photo-1462618.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+  ['E', 'S', 'S', 'A', 'M', 'E', 'B', 'T', 'G', 'B'],
+  'MESSAGE',
+  '',
+  'https://images.pexels.com/photos/1485548/pexels-photo-1485548.jpeg?auto=compress&cs=tinysrgb&w=600',
+  'https://images.pexels.com/photos/915051/pexels-photo-915051.jpeg?auto=compress&cs=tinysrgb&w=600',
+  'https://images.pexels.com/photos/210644/pexels-photo-210644.jpeg?auto=compress&cs=tinysrgb&w=600',
+  'https://images.pexels.com/photos/684318/pexels-photo-684318.jpeg?auto=compress&cs=tinysrgb&w=600',
+  'Words starts from MESSAG'
 ];
-
-// List level2 = [
-//   [
-//     '',
-//     '',
-//     '',
-//     '',
-//   ],
-//   ['A', 'B', 'L', 'C', 'L', 'A', 'B', 'L', 'C', 'L'],
-//   'BALL',
-//   'What is this?'
-// ];
+List level11 = [
+  ['', '', '', ''],
+  ['E', 'S', 'S', 'L', 'M', 'E', 'B', 'A', 'G', 'F'],
+  'LEAF',
+  '',
+  'https://images.pexels.com/photos/1122626/pexels-photo-1122626.jpeg?auto=compress&cs=tinysrgb&w=600',
+  'https://images.pexels.com/photos/6446708/pexels-photo-6446708.jpeg?auto=compress&cs=tinysrgb&w=600',
+  'https://images.pexels.com/photos/1028599/pexels-photo-1028599.jpeg?auto=compress&cs=tinysrgb&w=600',
+  'https://images.pexels.com/photos/6843561/pexels-photo-6843561.jpeg?auto=compress&cs=tinysrgb&w=600',
+  'Words starts from LEA'
+];
+List level12 = [
+  ['', '', '', ''],
+  ['I', 'B', 'S', 'A', 'Y', 'E', 'B', 'T', 'G', 'B'],
+  'BABY',
+  '',
+  'https://images.pexels.com/photos/12847983/pexels-photo-12847983.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
+  'https://images.pexels.com/photos/162318/cheetahs-cubs-two-together-162318.jpeg?auto=compress&cs=tinysrgb&w=600',
+  'https://images.pexels.com/photos/568022/pexels-photo-568022.jpeg?auto=compress&cs=tinysrgb&w=600',
+  'https://images.pexels.com/photos/5264001/pexels-photo-5264001.jpeg?auto=compress&cs=tinysrgb&w=600',
+  'Words starts from BAB'
+];
+List level13 = [
+  ['', '', '', ''],
+  ['E', 'R', 'S', 'A', 'O', 'E', 'B', 'D', 'G', 'B'],
+  'ROAD',
+  '',
+  'https://images.pexels.com/photos/18185784/pexels-photo-18185784/free-photo-of-old-town.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
+  'https://images.pexels.com/photos/18165531/pexels-photo-18165531/free-photo-of-motocross.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
+  'https://images.pexels.com/photos/3593923/pexels-photo-3593923.jpeg?auto=compress&cs=tinysrgb&w=600',
+  'https://images.pexels.com/photos/10640628/pexels-photo-10640628.jpeg?auto=compress&cs=tinysrgb&w=600',
+  'Words starts from ROA'
+];
+List level14 = [
+  ['', '', '', ''],
+  ['E', 'W', 'S', 'A', 'H', 'I', 'B', 'T', 'G', 'B'],
+  'WHITE',
+  '',
+  'https://images.pexels.com/photos/18156148/pexels-photo-18156148.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
+  'https://images.pexels.com/photos/4074444/pexels-photo-4074444.jpeg?auto=compress&cs=tinysrgb&w=600',
+  'https://images.pexels.com/photos/4821403/pexels-photo-4821403.jpeg?auto=compress&cs=tinysrgb&w=600',
+  'https://images.pexels.com/photos/4621783/pexels-photo-4621783.jpeg?auto=compress&cs=tinysrgb&w=600',
+  'Words starts from WHIT'
+];
+List level15 = [
+  ['', '', '', ''],
+  ['E', 'S', 'S', 'A', 'M', 'E', 'P', 'O', 'G', 'B'],
+  'POSE',
+  '',
+  'https://images.pexels.com/photos/1485548/pexels-photo-1485548.jpeg?auto=compress&cs=tinysrgb&w=600',
+  'https://images.pexels.com/photos/5262931/pexels-photo-5262931.jpeg?auto=compress&cs=tinysrgb&w=600',
+  'https://images.pexels.com/photos/1580271/pexels-photo-1580271.jpeg?auto=compress&cs=tinysrgb&w=600',
+  'https://images.pexels.com/photos/1321909/pexels-photo-1321909.jpeg?auto=compress&cs=tinysrgb&w=600',
+  'Words starts from POS'
+];
+List level16 = [
+  ['', '', '', ''],
+  ['E', 'S', 'S', 'A', 'M', 'E', 'B', 'L', 'G', 'B'],
+  'GLASSES',
+  '',
+  'https://images.pexels.com/photos/1211588/pexels-photo-1211588.jpeg?auto=compress&cs=tinysrgb&w=600',
+  'https://images.pexels.com/photos/928854/pexels-photo-928854.jpeg?auto=compress&cs=tinysrgb&w=600',
+  'https://images.pexels.com/photos/831889/pexels-photo-831889.jpeg?auto=compress&cs=tinysrgb&w=600',
+  'https://images.pexels.com/photos/258160/pexels-photo-258160.jpeg?auto=compress&cs=tinysrgb&w=600',
+  'Words starts from GLASSE'
+];
+List level17 = [
+  ['', '', '', ''],
+  ['L', 'B', 'S', 'A', 'U', 'E', 'B', 'T', 'G', 'B'],
+  'BULB',
+  '',
+  'https://images.pexels.com/photos/1108572/pexels-photo-1108572.jpeg?auto=compress&cs=tinysrgb&w=600',
+  'https://images.pexels.com/photos/568948/pexels-photo-568948.jpeg?auto=compress&cs=tinysrgb&w=600',
+  'https://images.pexels.com/photos/1853561/pexels-photo-1853561.jpeg?auto=compress&cs=tinysrgb&w=600',
+  'https://images.pexels.com/photos/5090644/pexels-photo-5090644.jpeg?auto=compress&cs=tinysrgb&w=600',
+  'Words starts from BUL'
+];
+List level18 = [
+  ['', '', '', ''],
+  ['E', 'S', 'S', 'A', 'M', 'O', 'B', 'P', 'G', 'B'],
+  'SOAP',
+  '',
+  'https://images.pexels.com/photos/905186/pexels-photo-905186.jpeg?auto=compress&cs=tinysrgb&w=600',
+  'https://images.pexels.com/photos/5938258/pexels-photo-5938258.jpeg?auto=compress&cs=tinysrgb&w=600',
+  'https://images.pexels.com/photos/5240615/pexels-photo-5240615.jpeg?auto=compress&cs=tinysrgb&w=600',
+  'https://images.pexels.com/photos/4202924/pexels-photo-4202924.jpeg?auto=compress&cs=tinysrgb&w=600',
+  'Words starts from SOA'
+];
+List level19 = [
+  ['', '', '', ''],
+  ['E', 'L', 'C', 'A', 'M', 'E', 'B', 'O', 'G', 'D'],
+  'COLD',
+  '',
+  'https://images.pexels.com/photos/813872/pexels-photo-813872.jpeg?auto=compress&cs=tinysrgb&w=600',
+  'https://images.pexels.com/photos/266679/pexels-photo-266679.jpeg?auto=compress&cs=tinysrgb&w=600',
+  'https://images.pexels.com/photos/789303/pexels-photo-789303.jpeg?auto=compress&cs=tinysrgb&w=600',
+  'https://images.pexels.com/photos/1438761/pexels-photo-1438761.jpeg?auto=compress&cs=tinysrgb&w=600',
+  'Words starts from COL'
+];
+List level20 = [
+  ['', '', '', ''],
+  ['E', 'S', 'S', 'A', 'M', 'E', 'B', 'T', 'G', 'B'],
+  'MESSAGE',
+  '',
+  'https://images.pexels.com/photos/1485548/pexels-photo-1485548.jpeg?auto=compress&cs=tinysrgb&w=600',
+  'https://images.pexels.com/photos/915051/pexels-photo-915051.jpeg?auto=compress&cs=tinysrgb&w=600',
+  'https://images.pexels.com/photos/210644/pexels-photo-210644.jpeg?auto=compress&cs=tinysrgb&w=600',
+  'https://images.pexels.com/photos/684318/pexels-photo-684318.jpeg?auto=compress&cs=tinysrgb&w=600',
+  'Words starts from MESSAG'
+];
